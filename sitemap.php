d4c330cd988ae40b55b76db314a7ce0128076e80
paulrr88 <paul@pauls-imac.home>
2019-03-07 15:13:49 +0000

    Preview stage setup

---
+++ sitemap.php
@@ -0,0 +1,53 @@
+
+<html lang="en">
+
+<head>
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Sitemap &mdash; 39 Youth Club Bognor</title>
+  <meta name="description" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- CSS & JavaScript
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css"><!-- reset file do not change -->
+  <link rel="stylesheet" href="css/skeleton.css"><!-- framework CSS -->
+  <link rel="stylesheet" href="css/style.css"><!--my additions-->
+  <link rel="stylesheet" href="css/breaks.css"><!--breakpoints of mine-->
+
+  <script>document.createElement('main');</script><!--fix for support of HTML5 <main> element-->
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+  <!-- Google fonts
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+  <!--<link href="https://fonts.googleapis.com/css?family=BenchNine" rel="stylesheet"><!--Bench Nine Google Font for headers-->
+</head>
+
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+
+    <main class="container clear overflow-hidden" role="main"><!--Container for ALL content, everything inside this-->
+        <h1 class="content-title-top content-title-pagetop">Sitemap</h1>
+        
+        <ul class="bold sitemap textmid3">
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">History</a></li>
+            <li><a href="">Our Policies</a></li>
+            <li><a href="">Events</a></li>
+            <li><a href="">Times &amp; Dates</a></li>
+            <li><a href="">Join Us</a></li>
+            <li><a href="">Volunteer</a></li>
+            <li><a href="">Contact Us</a></li>
+        </ul>
+    </main>
